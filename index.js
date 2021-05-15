f63694e86e034ddeaa2bc03fedd1925983fefb34
Kashish <kashishgoyal2019@u.northwestern.edu>
2021-05-15 17:05:32 -0400

    layout revision for posts

--- index.js
+++ index.js
@@ -1,86 +1,23 @@
-function Waves($canvas, $width, $height) {
-    this.numberOfWaves = 10;
-    this.waveGap = 20;
-    this.width = Waves.width = $width;
-    this.height = Waves.height = $height;
-    Waves.globalY = 0;
-    this.move = 1;
-    this.ctx = $canvas.getContext('2d');
+function generatePosts() {
 
-    this.colour = Math.round(Math.random() * 255) + ", " + Math.round(Math.random() * 255) + ", " + Math.round(Math.random() * 255);
+    fetch('assets/data.json')
+        .then(data => data.json())
+        .then((data) => {
+            const posts = data.posts;
+            var posts_div = document.getElementById('posts');
+            posts.forEach(post => {
+                const post_html = `<div class="card">
+                                <img src=${'assets/images/' + post.img_url} alt="Image">
+                                <div class="container">
+                                    <h4><b>${post.date}</b></h4>
+                                    <p>${post.description}</p>
+                                </div>
+                            </div>`
+                posts_div.innerHTML += post_html;
+            });
 
-    this.wavesArray = new Array();
+        })
 
-    this.beginingY = Waves.height / 1.1;
-    while (this.numberOfWaves--) {
-        this.wavesArray.push(new Wave($canvas, this.beginingY, this.colour));
-        this.beginingY += this.waveGap;
-    }
-
-    this.update = function () {
-        var bL = this.wavesArray.length;
-        while (bL--) {
-            this.wavesArray[bL].update();
-        }
-        Waves.globalY += this.move;
-        if (Waves.globalY > (Waves.height / 2) - 50) {
-            this.move = -1;
-        } else if (Waves.globalY < -(Waves.height / 2)) {
-            this.move = 1;
-        }
-    }
-
-    this.draw = function () {
-        this.ctx.save();
-        var bL = this.wavesArray.length;
-        while (bL--) {
-            this.wavesArray[bL].draw();
-        }
-        this.ctx.restore();
-    }
 }
 
-function Wave($canvas, $y, $colour) {
-    this.ctx = $canvas.getContext('2d');
-    this.force = 0;
-    this.wavePower = 40;
-    this.count = $y;
-    this.y = $y + Waves.globalY;
-    this.alpha = 0.1;
-
-    this.update = function () {
-        this.y = $y + Waves.globalY;
-        this.force = Math.sin(this.count);
-        this.count += 0.05;
-    }
-
-    this.draw = function () {
-        this.ctx.fillStyle = "rgba(255, 255, 255, 0.1)";
-        // this.ctx.fillRect(0, 0, Waves.width, Waves.height);
-        this.ctx.fillStyle = "rgba(" + $colour + ", " + this.alpha + ")";
-        this.ctx.beginPath();
-        this.ctx.moveTo(0, this.y);
-        this.ctx.quadraticCurveTo(Waves.width / 4, this.y + (this.wavePower * this.force), Waves.width / 2, this.y);
-        this.ctx.quadraticCurveTo(Waves.width * 0.75, this.y - (this.wavePower * this.force), Waves.width, this.y);
-        this.ctx.lineTo(Waves.width, Waves.height);
-        this.ctx.lineTo(0, Waves.height);
-        this.ctx.lineTo(0, this.y);
-        this.ctx.closePath();
-        this.ctx.fill();
-    }
-}
-
-var canvas = null;
-var waves = null;
-function loadCanvas() {
-    canvas = document.getElementById('canvas');
-    canvas.width = document.body.clientWidth; //document.width is obsolete
-    canvas.height = document.body.clientHeight; //document.height is obsolete
-    waves = new Waves(canvas, canvas.width, canvas.height);
-    setInterval("run()", 80);
-}
-function run() {
-    waves.update();
-    waves.draw();
-}
-window.addEventListener('load', loadCanvas, false);
+window.addEventListener('load', generatePosts, false);
